ccfa369ea35d0c03f15dfabbfa384b661a7b2eb2
Curtis Lytle <curtlytle@gmail.com>
2018-02-16 02:29:49 -0700

    fixed merge issue

--- client/src/Profile/ProfilePage.js
+++ client/src/Profile/ProfilePage.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Parallax from 'react-springy-parallax'
 import Banner from './Components/Banner'
 import About from './Components/About'
-import { Grid, Container, Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import {Grid, Container, Sidebar, Segment, Button, Menu, Image, Icon, Header} from 'semantic-ui-react'
 import {Link} from "react-router-dom"
 import ConnectButton from './Components/ConnectButton'
 import EmailButton from './Components/EmailButton'
@@ -14,7 +14,7 @@ import Bio from './Components/Bio'
 class ProfilePage extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             visible: false,
             first_name: "Alexander",
             last_name: "Walz",
@@ -36,95 +36,104 @@ class ProfilePage extends Component {
                 "Jquery",
                 "Bootstrap"
             ]
-         }
+        }
     }
 
-  
 
-    toggleVisibility = () => this.setState({ visible: !this.state.visible })
+    toggleVisibility = () => this.setState({visible: !this.state.visible})
 
-    render() { 
-        return ( 
-            
+    render() {
+        return (
 
-<<<<<<< HEAD
-        <div style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/greyzz.png')", backgroundPositionX: 'center', color:"grey", paddingTop: 80 }}>
-=======
-        <div style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/greyzz.png')", backgroundPositionX: 'center', color:"grey"}}>
 
-                
+            <div style={{
+                backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/greyzz.png')",
+                backgroundPositionX: 'center',
+                color: "grey"
+            }}>
+
+
                 <Sidebar.Pushable>
-                <Sidebar as={Menu} animation='overlay' width='thin' visible={this.state.visible} icon='labeled' vertical inverted style={{height: "100vh;"}}>
-                    <Menu.Item name='home'>
-                    <Link to="/" ><Icon name='home' />Home</Link>
-                    </Menu.Item>
-                    <Menu.Item name='gamepad'>
-                    <Link to="/profile/:id" ><Icon name='user' />My Profile</Link>
-                    </Menu.Item>
-                    <Menu.Item name='newspaper'>
-                    <Link to="/feed" ><Icon name='newspaper' />Feed</Link>
-                    </Menu.Item>
-                </Sidebar>
-                <Sidebar.Pusher>
->>>>>>> master
-                   
-                    <Container>
+                    <Sidebar as={Menu} animation='overlay' width='thin' visible={this.state.visible} icon='labeled'
+                             vertical inverted style={{height: "100vh;"}}>
+                        <Menu.Item name='home'>
+                            <Link to="/"><Icon name='home'/>Home</Link>
+                        </Menu.Item>
+                        <Menu.Item name='gamepad'>
+                            <Link to="/profile/:id"><Icon name='user'/>My Profile</Link>
+                        </Menu.Item>
+                        <Menu.Item name='newspaper'>
+                            <Link to="/feed"><Icon name='newspaper'/>Feed</Link>
+                        </Menu.Item>
+                    </Sidebar>
+                    <Sidebar.Pusher>
+
+                        <Container>
                             <Grid>
                                 <Grid.Row>
-                                <Grid.Column width={16}>
-                                    <Banner first_name={this.state.first_name} last_name={this.state.last_name} job_title={this.state.job_title} image_url={this.state.image_url}/>
-                                </Grid.Column>
+                                    <Grid.Column width={16}>
+                                        <Banner first_name={this.state.first_name} last_name={this.state.last_name}
+                                                job_title={this.state.job_title} image_url={this.state.image_url}/>
+                                    </Grid.Column>
                                 </Grid.Row>
 
 
-                                    <Grid.Row>
+                                <Grid.Row>
 
-                                     <Grid.Column width={9} >
+                                    <Grid.Column width={9}>
                                     </Grid.Column>
 
-                                    <Grid.Column width={3} >
-                                    <ConnectButton/>
+                                    <Grid.Column width={3}>
+                                        <ConnectButton/>
                                     </Grid.Column>
 
-                                    <Grid.Column width={3} >
+                                    <Grid.Column width={3}>
                                         <EmailButton email={this.state.email}/>
                                     </Grid.Column>
 
-                                    </Grid.Row>
-                            
+                                </Grid.Row>
+
                                 <Grid.Row>
-                                <Grid.Column width={8}>
+                                    <Grid.Column width={8}>
 
-                                            <Grid.Row>
+                                        <Grid.Row>
 
-                                                <Grid.Column width={16}>
-                                                    <Bio first_name={this.state.first_name} last_name={this.state.last_name} email={this.state.email} phone={this.state.phone} education={this.state.education} company={this.state.current_company} jobTitle={this.state.job_title} birthday={this.state.birthday} />
-                                                </ Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <Bio first_name={this.state.first_name} last_name={this.state.last_name}
+                                                     email={this.state.email} phone={this.state.phone}
+                                                     education={this.state.education}
+                                                     company={this.state.current_company}
+                                                     jobTitle={this.state.job_title} birthday={this.state.birthday}/>
+                                            </ Grid.Column>
 
-                                                <Grid.Column width={16}>
-                                                     <About about={this.state.about}/>
-                                                </ Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <About about={this.state.about}/>
+                                            </ Grid.Column>
 
-                                                <Grid.Column width={16}>
-                                                    <LanguagesView languages={this.state.languages}/>
-                                                </ Grid.Column>
+                                            <Grid.Column width={16}>
+                                                <LanguagesView languages={this.state.languages}/>
+                                            </ Grid.Column>
 
-                                                
-                                            </Grid.Row>
-                                    
-                                </Grid.Column>
-                                <Grid.Column width={8}>
-                                    <ProfileFeed first_name={this.state.first_name} last_name={this.state.last_name} image_url={this.state.image_url}/>
-                                </Grid.Column>
+
+                                        </Grid.Row>
+
+                                    </Grid.Column>
+                                    <Grid.Column width={8}>
+                                        <ProfileFeed first_name={this.state.first_name} last_name={this.state.last_name}
+                                                     image_url={this.state.image_url}/>
+                                    </Grid.Column>
 
                                 </Grid.Row>
-                        </Grid>
-                    </Container>     
-          </div>
+                            </Grid>
+                        </Container>
 
-        
-         )
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </div>
+
+
+        )
     }
 }
- 
+
 export default ProfilePage;
