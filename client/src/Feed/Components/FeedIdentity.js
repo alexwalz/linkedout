9e3ef26c1493e2ec7f3615bfaef078e1996c9b39
Cesar Caceres <cesarc770@gmail.com>
2018-02-16 14:48:03 -0700

    Added feed identity component

---
+++ client/src/Feed/Components/FeedIdentity.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Message, Grid, Segment, Divider, Header, Icon, Image } from 'semantic-ui-react'
+
+
+const FeedIdentity = (props) => (
+  <div style={{marginBottom:"10px"}}>
+    
+    <Segment>
+
+    <Header as='h2' icon textAlign='center' style={{color: "black"}}>
+      <Icon name='user' circular />
+      <Header.Content>
+        {props.first_name + " " + props.last_name}
+        <Header.Subheader>
+             <Divider horizontal style={{color: "#2185d0"}} >{props.job_title}</Divider>
+        </Header.Subheader>
+	   </Header.Content>
+    </Header>
+
+        
+    </Segment>
+  </div>
+)
+
+export default FeedIdentity;
--- client/src/Feed/FeedPage.js
+++ client/src/Feed/FeedPage.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-// import { Grid, Container, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import { Grid, Container, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import FeedIdentity from './Components/FeedIdentity';
 
 class FeedPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+        	first_name: "Alexander",
+            last_name: "Walz",
+            image_url: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAQDGAAwAAQAAAAAAAA7hAAAAJDE2YzA2NDBkLWU0YzMtNDE2Zi1hMDcxLTY0YWFiNWZlZjM5ZQ.jpg",
+            job_title: "Full Stack Developer"
+         }
     }
     render() { 
         return ( 
  			<div style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/greyzz.png')", backgroundPositionX: 'center', color:"grey", paddingTop: 80}}>
- 				<h1>Feed Page</h1>
+ 				 
+ 				 	
+ 				 	<Container>
+ 				 	<Grid>
+ 				 	<Grid.Row>
+ 				 		 <Grid.Column width={4}>
+ 							<FeedIdentity first_name={this.state.first_name} last_name={this.state.last_name}
+                             job_title={this.state.job_title} image_url={this.state.image_url}/>
+ 						</Grid.Column>
+ 						<Grid.Column width={8}>
+ 							
+ 						</Grid.Column>
+ 						<Grid.Column width={4}>
+ 							
+ 						</Grid.Column>
+ 					</Grid.Row>
+ 					</Grid>
+ 					</Container>
+
+ 					
+ 			
  			</div>
           
          )
