35f63b225eea117ed646a75a61c2bf867cff263e
alexwalz <alexwalz@icloud.com>
2018-02-22 14:05:31 -0700

    login page redirect if user is already logged in

--- client/src/Login/LoginPage.js
+++ client/src/Login/LoginPage.js
@@ -3,21 +3,43 @@ import Parallax from 'react-springy-parallax'
 import Logo from './Components/Logo'
 import LoginForm from './Components/LoginForm'
 import axios from 'axios'
+import {Redirect} from 'react-router-dom'
 
 
 
 
 class LoginPage extends Component {
 
+  state={
+    redirect: false,
+
+  }
+
   componentDidMount(){
+    this.getUser()
+  }
+
+
+  getUser = () => {
     axios.get('/api/users/login')
     .then(response => {
       console.log(response)
+      this.setState(response.data)
     })
     .catch(error => {
         console.log('Error fetching and parsing data', error);
     });
   }
+
+  redirect(){
+    const { from } = this.props.location.state || { from: { pathname: "/home/feed" } };
+    console.log("Logged In Already");
+    console.log(from);
+    <Redirect to={from}/>
+  }
+
+
+
 
 
   render() {
@@ -25,64 +47,68 @@ class LoginPage extends Component {
       display: 'flex', alignItems: 'center', justifyContent: 'center'
     }
 
+    return ( 
 
+        <Parallax ref='parallax' pages={2}>
 
-    return (
-      <Parallax ref='parallax' pages={2}>
-
-         
-        {/* Mobile  */}
-        <Parallax.Layer offset={0} speed={0} factor={4.1} style={{ backgroundImage: `url('http://brtweed.co.uk/dist/images/bt3.jpg')`, backgroundSize: 'cover', backgroundPositionX: 'center' }} />
-        <Parallax.Layer offset={1} speed={0} className='hide-on-med-and-up' onClick={() => this.refs.parallax.scrollTo(0)} />
-
-        <Parallax.Layer offset={0} speed={0} style={{ backgroundImage: `url(${'http://brtweed.co.uk/dist/images/bt3.jpg'})`, backgroundSize: 'cover' }} />
-        <Parallax.Layer offset={1} speed={0} style={{ backgroundImage: `url(${'http://getwallpapers.com/wallpaper/full/2/b/b/488958.jpg'})`, backgroundSize: 'cover' }} />
-
-
-        <Parallax.Layer
-          offset={0}
-          speed={0}
-        >
-          {/* stuff for First page */}
+        {this.getUser}
+        {console.log(this.state)}
+        {(this.state.loggedIn) ? <Redirect to="/home/feed"/> : null}
+  
+           
+          {/* Mobile  */}
+          <Parallax.Layer offset={0} speed={0} factor={4.1} style={{ backgroundImage: `url('http://brtweed.co.uk/dist/images/bt3.jpg')`, backgroundSize: 'cover', backgroundPositionX: 'center' }} />
+          <Parallax.Layer offset={1} speed={0} className='hide-on-med-and-up' onClick={() => this.refs.parallax.scrollTo(0)} />
+  
+          <Parallax.Layer offset={0} speed={0} style={{ backgroundImage: `url(${'http://brtweed.co.uk/dist/images/bt3.jpg'})`, backgroundSize: 'cover' }} />
+          <Parallax.Layer offset={1} speed={0} style={{ backgroundImage: `url(${'http://getwallpapers.com/wallpaper/full/2/b/b/488958.jpg'})`, backgroundSize: 'cover' }} />
+  
+  
           <Parallax.Layer
             offset={0}
-            speed={0.5}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
-            onClick={() => this.refs.parallax.scrollTo(1)}>
-          </Parallax.Layer>
-
-          <Parallax.Layer
-            offset={0.0}
-            speed={0.7}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
-            onClick={() => this.refs.parallax.scrollTo(1)}>
-      
-              <Logo/>
-         
-          </Parallax.Layer>
-
-        </Parallax.Layer>
-
-
-
-
-        {/* stuff for Second page */}
-        <Parallax.Layer
-          offset={1}
-          speed={-0.1}
-          style={styles}
-          onClick={() => this.refs.parallax.scrollTo(0)}>
-        </Parallax.Layer>
-
-        <Parallax.Layer
-          offset={1.03}
-          speed={2.9}
+            speed={0}
           >
-          <LoginForm/>
-        </Parallax.Layer>
-
-      </Parallax>
-    );
+            {/* stuff for First page */}
+            <Parallax.Layer
+              offset={0}
+              speed={0.5}
+              style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
+              onClick={() => this.refs.parallax.scrollTo(1)}>
+            </Parallax.Layer>
+  
+            <Parallax.Layer
+              offset={0.0}
+              speed={0.7}
+              style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
+              onClick={() => this.refs.parallax.scrollTo(1)}>
+        
+                <Logo/>
+           
+            </Parallax.Layer>
+  
+          </Parallax.Layer>
+  
+  
+  
+  
+          {/* stuff for Second page */}
+          <Parallax.Layer
+            offset={1}
+            speed={-0.1}
+            style={styles}
+            onClick={() => this.refs.parallax.scrollTo(0)}>
+          </Parallax.Layer>
+  
+          <Parallax.Layer
+            offset={1.03}
+            speed={2.9}
+            >
+            <LoginForm/>
+          </Parallax.Layer>
+  
+        </Parallax>
+      );
+  
   }
 }
 
