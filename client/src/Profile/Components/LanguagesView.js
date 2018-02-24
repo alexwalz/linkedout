da43364fc8864e111ef2afb7086870a3310ac5d2
Cesar Caceres <cesarc770@gmail.com>
2018-02-24 11:37:25 -0700

    language view changes

--- client/src/Profile/Components/LanguagesView.js
+++ client/src/Profile/Components/LanguagesView.js
@@ -17,9 +17,7 @@ class LanguagesView extends Component {
   constructor(props) {
         super(props);
         this.state = { 
-          editing: false,
-          limit: true,
-          loading: true
+          editing: false
          }
     }
 
@@ -27,33 +25,16 @@ class LanguagesView extends Component {
       this.setState({editing: true})
     }
 
-    cancel(){
-      this.setState({editing: false})
-      this.setState({limit: true})
-    }
-
     save(){
       this.setState({editing: false})
-      this.setState({limit: true})
-      //api route to save new array of languages
-    }
-
-    removeLimit(){
-      this.setState({limit: false})
-    }
-
-    limit(){
-      this.setState({limit: true})
     }
 
     componentDidMount(){
-      setTimeout(() => this.setState({ loading: false }), 1500); 
+      
     }
 
     renderForm(){
-       let onSort = function(sortedList, dropEvent) {
-    this.setState({languages: sortedList})
-          }
+    
       return(
           <div>
     
@@ -67,12 +48,8 @@ class LanguagesView extends Component {
               </Grid.Row>
                 <Form>
                   <Form.Input fluid placeholder="Add another language"/>
-                  <Button icon='check' size="large" circular color='teal'/>
-                  <Icon name='close' size='large' circular onClick={()=> this.save()} />
-                </Form>
-
-                <Header textAlign="center" style={{color:"grey"}}>Drag and Drop to reorder your languages</Header>
-                <DragSortableList items={this.props.languagesDrag} dropBackTransitionDuration={0.3} type="vertical"/>
+                  <Button icon='check' size="large" circular color='teal' onClick={()=> this.save()} />
+              </Form>
           </Segment>
         </div>
         )
@@ -80,55 +57,7 @@ class LanguagesView extends Component {
 
     renderDisplay(){
       return(
-          <Container>
-            {(this.state.limit) ? this.renderLimit() : this.renderNoLimit()}
-            </Container>
-        )
-    }
-
-    renderLimit(){
-      const loading  = this.state.loading;
-
-       if(loading) { 
-        return (
-          <div>
-          <Container>
-            <Segment  style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/always_grey.png')"}}>
-              <Divider horizontal><h3  textAlign="center" style={{color: "white"}}>My Languages</h3></Divider>
-  
-              <Loader active inline='centered' size="big" inverted style={{marginTop: "5%"}}/>
-  
-            </Segment>
-        </Container>
-      </div>
-        )
-      }
-
-      return(
-        <div>
-    
-          <Segment  style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/always_grey.png')"}}>
-              <Grid.Row style={{marginBottom: "10px", marginBottom: "10px"}}>
-
-                {this.props.loggedInUser === this.props.url ? <Icon name='pencil' color="white" size='small' onClick={()=> this.edit()} /> : null }
-                <Grid.Column textAlign="center">
-                <Divider horizontal><h3  textAlign="center" style={{color: "white"}}>My Languages</h3></Divider>
-                </Grid.Column>
-              </Grid.Row>
-
-                      {this.props.limit5.map((language, key) => (
-                        <Languages language={language} key={key} />
-                      ))}
-              <p onClick={()=>this.removeLimit()} style={{cursor: "pointer", color: "white", marginTop: "5px"}}>View All Languages</p>
-          </Segment>
-          
-        </div>
-      )
-    }
-
-    renderNoLimit(){
-      return(
-        <div>
+           <div>
     
           <Segment  style={{backgroundImage: "url('https://www.toptal.com/designers/subtlepatterns/patterns/always_grey.png')"}}>
               <Grid.Row style={{marginBottom: "10px"}}>
@@ -142,11 +71,13 @@ class LanguagesView extends Component {
                       {this.props.languages.map(language => (
                         <Languages language={language} />
                       ))}
-              <p onClick={()=>this.limit()}  style={{cursor: "pointer", color: "white", marginTop: "5px"}}>Hide</p>
+              
           </Segment>
         </div>
-      )
+        )
     }
+
+   
 
   render() { 
     return(
