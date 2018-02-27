7a0fa44233ffddd4ae054eef1d2468edd6b3179a
Cesar Caceres <cesarc770@gmail.com>
2018-02-27 11:58:26 -0700

    changes to feed connections

--- client/src/Feed/FeedPage.js
+++ client/src/Feed/FeedPage.js
@@ -23,31 +23,7 @@ class FeedPage extends Component {
                     date:""
                 }],
             connections:[
-                {
-                    name: "Alex Walz",
-                    image_url: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAQDGAAwAAQAAAAAAAA7hAAAAJDE2YzA2NDBkLWU0YzMtNDE2Zi1hMDcxLTY0YWFiNWZlZjM5ZQ.jpg",
-                    id: "12345",
-                },
-                {
-                    name: "Kurt Roberts",
-                    image_url: "https://media.licdn.com/media/AAEAAQAAAAAAAAxcAAAAJGY4MzNmNmMzLWMwM2QtNGQ4Ny1hYWZhLTQ0MTkzYzA5NzRiYQ.jpg",
-                    id: "234lkjsdf",
-                },
-                {
-                    name: "Cesar Caceres",
-                    image_url: "https://media.licdn.com/media/AAMABADGAAwAAQAAAAAAABCdAAAAJGE2ZDM1NjAzLWQzN2QtNGViMS05MWY0LWQxNzBjNDgyOTE3Mg.jpg",
-                    id: "asldkjfsdfj",
-                },
-                {
-                    name: "Andrew Reyes-Cairo",
-                    image_url: "https://media.licdn.com/media/AAEAAQAAAAAAAAwJAAAAJDQyNTQ2NDcyLTdkODItNDM0Ni1iMDVmLThmYWM3NTg5YzdmMw.jpg",
-                    id: "asldfusdofijsdf",
-                },
-                {
-                    name: "Curtis Lytle",
-                    image_url: "https://media.licdn.com/media/AAMABADuAAgAAQAAAAAAAA9ZAAAAJDM1YWEwYWQ3LTFmOWEtNDQ0Yi1hNDgxLTJkMTNjZTZkMTdmNQ.bin",
-                    id: "23423lkjfs443w32",
-                },
+               
             ]
          }
     }
@@ -71,6 +47,7 @@ class FeedPage extends Component {
             this.setState({lastName: response.data.userData.lastName});
             this.setState({job_title: response.data.userData.job_title});
             this.setState({image_url: response.data.userData.image_url});
+            this.setState({connections: response.data.userData.connections});
             this.setState({loggedInUser: response.data });             
         })
         .catch(error => {
