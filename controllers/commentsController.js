6f26409a5836bd33c9b36462df2c51aae142ec5c
Curtis Lytle <curtlytle@gmail.com>
2018-03-07 21:21:45 -0700

    added update and removal routes for comments

---
+++ controllers/commentsController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function (req, res) {
+        db.comment
+            .find({})
+            .then(function (dbModel) {
+                res.json(dbModel);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    },
+    findById: function (req, res) {
+        db.comment
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.comment
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.comment
+            .findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.comment
+            .findById({_id: req.params.id})
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
