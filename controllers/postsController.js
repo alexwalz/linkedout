b9232d2e9080634835878dd85300b9ebc77c4ded
Curtis Lytle <curtlytle@gmail.com>
2018-03-07 20:18:00 -0700

    populating comments

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -14,6 +14,7 @@ module.exports = {
     findById: function (req, res) {
         db.Post
             .findById(req.params.id)
+            .populate('comments')
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
