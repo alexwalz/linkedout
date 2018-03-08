65943a82a9c9eca15d2f2aa3d54f12992c358909
Curtis Lytle <curtlytle@gmail.com>
2018-03-07 21:02:55 -0700

    Added /api/posts/:id to put to update a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
 // Matches with "/api/posts"
-router.route("/")
-    .post(function(req, res) {
-        usersController.addPost(req, res);
+router.route("/:id")
+    .put(function(req, res) {
+        postController.update(req, res);
     });
 
 module.exports = router;
