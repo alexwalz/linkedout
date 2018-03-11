eb8abe8b24d24132d5cc8bb40a01bd1c7462029f
alexwalz <alexwalz@icloud.com>
2018-03-10 23:42:03 -0700

    ability to only connect one time with each person!

--- client/src/Profile/Components/ConnectButton.js
+++ client/src/Profile/Components/ConnectButton.js
@@ -7,6 +7,7 @@ class ConnectButton extends Component {
   constructor(props) {
     super(props);
     this.state = { 
+      updateReady: true,
       connected: false
      }
   }
@@ -21,18 +22,63 @@ class ConnectButton extends Component {
     )
     .then(r => {
       console.log(r)
+      this.props.renderUser()
+      this.setState({connected: true})
     })
     .catch(e => console.log(e));
   }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.updateReady === true){
+      this.handleConnectionCheck()
+    }else {
+      console.log("Done")
+    }
+    console.log(this.state)
+  }
+
+  handleConnectionCheck = () =>{
+    this.props.loggedInUser.loggedIn ? 
+    this.props.loggedInUser.userData.connections.map((connectionCheck, key) => (
+      connectionCheck._id === this.props.url ? this.setState({connected: true, updateReady: false}) : console.log("Not Connected")
+    ))
+    
+    : console.log(false)
+  }
+
+  connectedButton = () => {
+    return(
+      <Button basic color="red"><Icon name='heart'/>Connected</Button>
+    )
+  }
+
+
+  connectButton = () => {
+    return(
+      <Button basic color="grey" onClick={this.handleConnect}><Icon name='empty heart'/>Connect</Button>
+    )
+  }
+
+
 
 
   render(props) { 
     return ( 
       <div>
+
+    {/* {this.props.loggedInUser.loggedIn ? 
+    this.props.loggedInUser.map((connectionCheck, key) => (
+      console.log(connectionCheck)
+    ))}
+     : console.log(false); */}
+
+     {/* {this.props.loggedInUser.loggedIn ? this.handleConnectionCheck : console.log(false)} */}
+
+
           <Button.Group>
-              <Button basic color="grey" onClick={this.handleConnect}> Connect </Button>
+              {this.state.connected ? this.connectedButton() : this.connectButton()}
               <Button.Or />
-              <Button basic color="teal">Email Me</Button>
+              <Button basic color="grey"><Icon name='mail'/>Email Me</Button>
         </Button.Group>
     </div>
      )
