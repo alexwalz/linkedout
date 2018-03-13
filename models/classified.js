5724a39c1c4002cce8c1c9e5a3095f566a4dfcc2
Curtis Lytle <curtlytle@gmail.com>
2018-03-12 21:32:13 -0600

    added classified routes

--- models/classified.js
+++ models/classified.js
@@ -2,32 +2,92 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const classifiedschema = new Schema({
-    contactName: {
+    contact_name: {
         type: String,
         required: true,
         trim: true
     },
-    contactEmail: {
+    contact_email: {
         type: String,
         required: false,
         trim: true
     },
-    contactPhone: {
+    phone_number: {
         type: String,
         required: false,
         trim: true
     },
-    positionDescription: {
+    additional_info: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    job_title: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    company_name: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    city: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    state: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    zip: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    position_description: {
         type: String,
         required: true,
         trim: true
     },
-    positionResponsibilities: {
+    position_responsibilities: {
         type: String,
         required: false,
         trim: true
     },
-    positionQualifications: {
+    position_qualifications: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    salaryFrom: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    salaryTo: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    jobType: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    eduLevel: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    yearsOfExp: {
         type: String,
         required: false,
         trim: true
